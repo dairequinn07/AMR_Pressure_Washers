44465994cef32dfe596d6c3d31ad070e45071843
Daire Quinn <dairequinn@hotmail.co.uk>
2024-11-25 15:32:23 +0000

    Navbar changes. Squareup implementation. Adding delete button to cart

--- main.py
+++ main.py
@@ -79,14 +79,12 @@ def generate_checkout():
         return jsonify({'error': 'Unable to generate payment link'}), 500
 
 
-
-
 # Step 3: Add HTTPS redirection before any request is processed
-@app.before_request
-def https_redirect():
-    if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
-        # Redirect HTTP requests to HTTPS
-        return redirect(request.url.replace('http://', 'https://', 1), code=301)
+# @app.before_request
+# def https_redirect():
+#     if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
+#         # Redirect HTTP requests to HTTPS
+#         return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
 
 @app.before_request
@@ -200,6 +198,19 @@ def MyCart():
     cart = session.get('cart', [])
     subtotal = sum(item['Price'] * item['Quantity'] for item in cart)
     return render_template('MyCart.html', cart=cart, subtotal=subtotal)
+
+
+@app.route('/remove_from_cart', methods=['POST'])
+def remove_from_cart():
+    data = request.get_json()
+    item_id = data.get('id')  # Get the item ID from the request
+
+    if 'cart' in session:
+        # Filter the cart to remove the item with the given ID
+        session['cart'] = [item for item in session['cart'] if item['ID'] != item_id]
+        session.modified = True  # Mark the session as modified
+        return jsonify({"success": True, "message": "Item removed from cart"})
+    return jsonify({"success": False, "message": "Cart not found"}), 404
 
 
 @app.route("/", methods=['GET'])
