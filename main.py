cd65d686d6eb009dd4727e80e8b16d377f881cb6
Daire Quinn <dairequinn@hotmail.co.uk>
2024-11-26 18:36:13 +0000

    Css changes!

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ client = Client(access_token="EAAAl9pSAnbXNUGkFdWUy1Y3-j7DOntq6DNaZfdf5OhaBJxWgp
 
 
 # Step 3: Add HTTPS redirection before any request is processed
-# @app.before_request
-# def https_redirect():
-#     if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
-#         # Redirect HTTP requests to HTTPS
-#         return redirect(request.url.replace('http://', 'https://', 1), code=301)
+@app.before_request
+def https_redirect():
+    if not request.is_secure and request.headers.get('x-forwarded-proto') != 'https':
+        # Redirect HTTP requests to HTTPS
+        return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
 
 @app.before_request
